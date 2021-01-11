1191fc751cc2c7afc0d31156713a2747780d3d33
Sowmya Dhanapal <sowmya@multicorewareinc.com>
2021-01-11 18:32:03 +0530

    MetaWareNN-EP: Remove unused passes

--- onnxruntime/core/providers/metawarenn/metawarenn_lib/optimizer/pass_manager.cc
+++ onnxruntime/core/providers/metawarenn/metawarenn_lib/optimizer/pass_manager.cc
@@ -8,13 +8,7 @@ PassManager::PassManager() { std::cout << "\n In PassManager Constructor!!"; }
 void PassManager::run_passes() {
   for (auto& pass : pass_list) {
     std::cout  << "\n PassName : " << pass->get_name();
-    if(auto pass1 = std::dynamic_pointer_cast<DummyPass1>(pass)) {
-      std::cout << "\n Value : " << pass1->get_value();
-    }
-    else if (auto pass2 = std::dynamic_pointer_cast<DummyPass2>(pass)) {
-      std::cout << "\n Value : " << pass2->get_value();
-    }
-    else if (auto remove_reshape_pass = std::dynamic_pointer_cast<RemoveReshape>(pass)) {
+    if (auto remove_reshape_pass = std::dynamic_pointer_cast<RemoveReshape>(pass)) {
       remove_reshape_pass->RunPass();
     }
     else if (auto fuse_relu_pass = std::dynamic_pointer_cast<FuseRelu>(pass)) {
